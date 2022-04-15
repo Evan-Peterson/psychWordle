5ff8ba441253c70270d210d71102acdef92a1c4d
Evan-Peterson <4epeterson99@gmail.com>
2022-04-14 20:52:26 -0500

    added box guesses and keypressed

--- Box.js
+++ Box.js
@@ -1,11 +1,13 @@
 class Box {
 
-    constructor(x, y, answer) {
+    constructor(x, y, answer, word) {
         this.x = x;
         this.y = y;
         this.answer = answer.toUpperCase();
         // 0 default, 1 (full grey) not in word, 2 (full yellow) in word wrong place, 3 (full green) right letter right place
         this.status = 0;
+        this.guess = "";
+        this.word = word;
     }
 
     display() {
@@ -25,7 +27,7 @@ class Box {
         rect(this.x, this.y, 60);
         fill('#FFFFFF');
         textSize(36);
-        text(this.answer, this.x + 19, this.y + 43);
+        text(this.guess, this.x + 19, this.y + 43);
     };
 
 
@@ -33,6 +35,18 @@ class Box {
         this.status = newStatus;
     };
 
+    setGuess(newGuess) {
+        this.guess = newGuess;
 
+        if(this.guess === this.answer) {
+            this.status = 3;
+        } else {
+            if(this.word.includes(newGuess)) {
+                this.status = 2;
+            } else {
+                this.status = 1;
+            }
+        }
+    };
 
 }
--- sketch.js
+++ sketch.js
@@ -2,26 +2,33 @@
 let questions = ["this is a test question"];
 let answers = ["answer"];
 let boxes = [];
+let currentGuess = 0;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background('#121213');
     // background('#FFFFFF');
 
-    // var tempBox = new Box(500, 500, "a");
-    // tempBox.setStatus(3);
-    // tempBox.display();
+    
 
     let split = answers[0].split("");
-    console.log(split);
-    for(var i = 0;i < answers[0].length;i++) {
-        var x = (windowWidth / 2) + (60 * i) + (5 * i);
-        // if(i % 2 != 0) {
-        //     x += 5;
-        // } 
-        // var y = (windowHeight / 2) + (60 * i) + 5
-        boxes[i] = new Box(x, (windowHeight / 2), split[i]);
+
+    for(var i = 0;i < 5;i++) {
+        boxes[i] = [];
     }
+
+    let offset = (answers[0].length  / 2) * 60; 
+    
+    for(var k = 0;k < 5;k++) {
+        var y = (windowHeight / 6) + (60 * k) + (10 * k);
+        for(var i = 0;i < answers[0].length;i++) {
+            var x = (windowWidth / 2) + (60 * i) + (5 * i) - offset;
+    
+            
+            boxes[k][i] = new Box(x, y, split[i], answers[0]);
+        }
+    }
+
 
 
 }
@@ -29,6 +36,14 @@ function setup() {
 
 function draw() {
     for(var i = 0;i < boxes.length;i++) {
-        boxes[i].display();
+        for(var k = 0;k < boxes[0].length;k++) {
+            boxes[i][k].display();
+        }
+        
     }
+}
+
+
+function keyPressed() {
+    console.log(key);
 }
